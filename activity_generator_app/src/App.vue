157818be89132c8cee0a1add8903f8e07d94cc31
redfiretim <timscheepens@gmail.com>
2020-06-29 17:48:33 +0200

    removing vue dummy content

--- activity_generator_app/src/App.vue
+++ activity_generator_app/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <p>Hello Tim :)</p>
   </div>
 </template>
+
+
+
+<script>
+export default {
+  
+}
+
+</script>
+
+
+
 
 <style>
 #app {
@@ -14,19 +22,8 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: black;
+  margin-top: 0px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
