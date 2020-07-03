7cd058f6083db44323c3c557ccef687ed7944ea3
redfiretim <timscheepens@gmail.com>
2020-07-03 22:30:44 +0200

    result page mostly finished.
    minor tweaks maybe needed for final.

--- activity_generator_app/src/components/ResultComponent.vue
+++ activity_generator_app/src/components/ResultComponent.vue
@@ -6,10 +6,18 @@
       <div class="participants-label">Participants:</div>
       <ul class="participants_icons">
         <!-- array index used, because no other unique way to identify -->
-        <li v-for="(person, index) in peopleIcons" :key="index">
+        <li v-for="(person, index) in peopleIcons" :key="index" :class="index + 'icon'">
           <img :src="person" />
         </li>
       </ul>
+      <div class="accessibility-label">Accessibility:</div>
+      <div class="access-bar-container">
+        <div class="access-bar-inner" :style="{ width: accessPerc + '%'}"></div>
+      </div>
+      <div class="price-label">Price:</div>
+      <div class="price-bar-container">
+        <div class="price-bar-inner" :style="{ width: pricePerc + '%'}"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -30,27 +38,25 @@ export default {
       error: Boolean,
       peopleIcons: [grey, grey, grey, grey, grey],
       grey: grey,
-      mint: mint
+      mint: mint,
+      accessPerc: null,
+      pricePerc: null
     };
   },
   mounted: function() {
     this.activityData = this.output;
     this.error = this.errored;
-    //SANITYCHECK
-    console.log("mounted!");
-    console.log(this.activityData.participants + " <-- participants");
     
-    var peopleArray = this.peopleIcons;
+    //changes person icon into active mint color according to numer of participants
     for (let i = 0; i < this.activityData.participants; i++) {
-      //SANITYCHECK
-      console.log(i + " <-- i");
-      
-      peopleArray[i] = this.mint;
+      this.peopleIcons[i] = this.mint;
     }
-    //SANITYCHECK
-    console.log(this.peopleIcons + " <-- peopleIcons");
-    
-    
+
+    //changes accessibility data into percentage for css width usage
+    this.accessPerc = (this.activityData.accessibility * 100);
+
+    //changes price data into percentage for css width usage
+    this.pricePerc = (this.activityData.price * 100);
   },
 };
 </script>
@@ -62,7 +68,7 @@ export default {
 .container-result {
   display: grid;
   grid-template-rows: repeat(11, 1fr);
-  height: 100%;
+  max-height: 18rem;
 }
 
 .activity-title {
@@ -94,5 +100,75 @@ export default {
   grid-row: 2;
   text-align: left;
   padding: 5px 0;
+}
+
+/* removes parent container as constraint and places list items on grid */
+.participants_icons{
+  display: contents;
+}
+
+/* places list items on correct grid location */
+.participants_icons>li:first-child{
+  grid-column: 2;
+}
+.participants_icons>li:nth-child(2){
+  grid-column: 3;
+}
+.participants_icons>li:nth-child(3){
+  grid-column: 4;
+}
+.participants_icons>li:nth-child(4){
+  grid-column: 5;
+}
+.participants_icons>li:nth-child(5){
+  grid-column: 6;
+}
+
+li{
+  grid-row: 3;
+}
+
+.accessibility-label{
+  grid-column-start: 2;
+  grid-column-end: 5;
+  grid-row: 4;
+  text-align: left;
+  padding: 15px 0 0 0;
+}
+
+.access-bar-container{
+  background-color: #e6e6e6;
+  grid-column-start: 2;
+  grid-column-end: 10;
+  grid-row: 5;
+  margin: 0.3rem 0;
+}
+
+.access-bar-inner{
+  background-color: #3cf1aa;
+  height: 100%;
+  min-height: 1rem;
+}
+
+.price-label{
+  grid-column-start: 2;
+  grid-column-end: 5;
+  grid-row: 6;
+  text-align: left;
+  padding: 15px 0 0 0;
+}
+
+.price-bar-container{
+  background-color: #e6e6e6;
+  grid-column-start: 2;
+  grid-column-end: 10;
+  grid-row: 7;
+  margin: 0.3rem 0;
+}
+
+.price-bar-inner{
+  background-color: #3cf1aa;
+  min-height: 1rem;
+  height: 100%;
 }
 </style>
